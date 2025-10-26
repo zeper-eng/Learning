f785afd0a2906aec69e05beb7c81d554a21c639c
Luis Perez <luis@Luiss-MacBook-Pro.local>
2025-10-26 19:23:57 -0400

    installed node.js and then typescripts extension

--- The_Basics_TypeScript_Handbook.ts
+++ The_Basics_TypeScript_Handbook.ts
@@ -4,11 +4,11 @@
 
 // here we are defining message as a constant
 // what does that mean? this means it is "block-scoped", read-only
-
-const message = "Hello World!"; // this is a string literal type, because the variable is const and holds a primitive literal typescript narrows its type to the exact value "hello" (a subtype of a string)
-
+const message = "Hello World!"; // this is a string literal type, as in the type is "hello world" a specific string, not the type string
+console.log(message);
 
 // we cannot re-assign it, but lets say it was an array or an "object" we can still mutate its contents (unless its frozen)
 // for example this would be allowed
 const user = { name: "Ada" };
 user.name = "Grace"; 
+console.log(user);
